b01cc31f134678ced128fe83a688972fe4ceb203
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2025-11-27 21:28:06 +0530

    Add files via upload

---
+++ Q11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a Number %d",num);
+	scanf("%d",&num);
+	
+	if (num%2==0)
+	printf("Number is EVEN");
+	
+	else 
+	printf("Number is ODD");
+	
+	return 0;
+}
